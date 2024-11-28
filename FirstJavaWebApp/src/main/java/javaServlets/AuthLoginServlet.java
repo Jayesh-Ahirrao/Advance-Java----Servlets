e5d019f898b2ce1106d659bb375fa6cd04f9d633
Jayesh Ahirrao <ahirraojayesh88@gmail.com>
2024-11-28 15:58:39 +0530

    Servlet II : Assignment 01 - Login using hardcoded values

---
+++ FirstJavaWebApp/src/main/java/javaServlets/AuthLoginServlet.java
@@ -0,0 +1,46 @@
+package javaServlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AuthLoginServlet
+ */
+@WebServlet("/authLogin")
+public class AuthLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String uid = request.getParameter("uid");
+		String pwd = request.getParameter("pwd");
+		PrintWriter out = response.getWriter();
+		
+		if(uid.equals("jayesh") && pwd.equals("password")) {
+			out.println("Login Successful");
+		}else {
+			out.println("Login Failed");
+		}
+	}
+
+}
